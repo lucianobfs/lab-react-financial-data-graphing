52c991529b50ef5255a0b132b84065120f6feea2
Luciano <lucianobfsantana@hotmail.com>
2021-11-16 19:33:38 -0300

    done lab

---
+++ src/components/Graph.js
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import Chart from "chart.js/auto";
+import axios from "axios";
+
+import InputDate from "./InputDate";
+
+
+
+function Graph() {
+    const [priceData, setPriceData] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [chart, setChart] = useState(null);
+
+    const [data, setData] = useState({})
+
+    const [link, setLink] = useState(`http://api.coindesk.com/v1/bpi/historical/close.json`)
+
+    console.log(data)
+
+    useEffect(() => {
+        setLoading(true)
+        axios.get(link).then((response) => {
+            setPriceData({...response.data.bpi})
+            console.log(priceData)
+            setLoading(false)
+        }).catch((err) => {
+            console.log(err)
+            setLoading(false)
+        })
+
+    
+        
+
+    }, [data, link])
+
+    useEffect(() => {
+        if (data !== {}) {
+
+            // if (chart) {
+            //     chart.destroy()
+            // }
+
+          setLink(
+            `https://api.coindesk.com/v1/bpi/historical/close.json?start=${data.initialDate}&end=${data.finalDate}&currency=${data.currency}`
+          );
+        }
+      }, [data]);
+
+
+    useEffect(() => {
+        if (!loading) {
+            function renderChart() {
+                let ctx = document.getElementById('myCanvas').getContext('2d')
+
+                if (chart) {
+                    chart.destroy()
+                }
+
+                let chartInstance = new Chart(ctx, {
+                    // width: "100px",
+                    // height: "100px",
+                    type: 'line',
+                    data: {
+                        labels: Object.keys(priceData),
+                        datasets: [
+                            {
+                                label: "Bitcoin Price $BTC",
+                                data: Object.values(priceData),
+                                borderColor: "#0330fc",
+                                backgroundColor: "#03b1fc",
+                                fill: true,
+                            },
+                        ],
+                    },
+                })
+
+                setChart(chartInstance)
+            }
+
+            renderChart()
+        }
+    }, [loading, priceData, data])
+
+    return (
+        <div>
+
+            <div  className="d-flex justify-content-around align-items-end">
+                <InputDate setData={setData}/>
+
+            </div>
+        
+            {loading ? <div className="spinner-border" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div> : <canvas id="myCanvas" className="mt-5" />}
+        </div>
+    )
+
+}
+
+
+
+
+export default Graph
---
+++ src/components/InputDate.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import Chart from "chart.js/auto";
+import axios from "axios";
+
+
+
+function InputDate(props) {
+
+    let [formData, setFormData] = useState({
+        initialDate: '',
+        finalDate: '',
+        currency: 'USD'
+    })
+
+
+
+    function handleChange(event) {
+        let value = event.target.value
+
+        let valueOpt = event.target.option
+
+        setFormData({...formData, [event.target.name]: value || valueOpt})
+
+        // console.log(formData)
+    }
+
+    function handleSubmit(event) {
+
+
+        event.preventDefault()
+
+        console.log(formData)
+
+        props.setData(formData)
+       
+    }
+
+
+
+    return (
+        <div className="d-flex d-row align-items-center">
+
+        <form onSubmit={handleSubmit}>
+
+            <label className="label">
+                Start: 
+                <input className="form-control" type="date" name="initialDate" value={formData.initialDate} onChange={handleChange}/>
+            </label>
+
+
+            <label className="label">
+                End: 
+                <input className="form-control" type="date" name="finalDate" value={formData.finalDate} onChange={handleChange}/>
+            </label>
+
+            <select class="form-select" aria-label="Default select example" onChange={handleChange} name="currency">
+                <option selected value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="BRL" >BRL</option>
+            </select>
+
+            <button type="submit"  className="btn btn-light">Filter</button>
+
+
+        </form>
+
+
+
+        </div>
+    )
+
+}
+
+
+
+export default InputDate
